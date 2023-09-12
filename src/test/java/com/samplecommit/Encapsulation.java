5895f30fe785a7981d1963b1782366a86bba45f9
kamat <kamat@LAPTOP-B5VT0PNK>
2023-09-12 12:34:36 +0200

    Encapsulation-commit

---
+++ src/test/java/com/samplecommit/Encapsulation.java
@@ -0,0 +1,18 @@
+package com.samplecommit;
+
+public class Encapsulation {
+
+	private String lastname;
+	private String firstname;
+	
+	public void setconcat(String lastname,String  firstname ) {
+		this.lastname=lastname;
+		this.firstname=firstname;
+		
+		}
+	public String getconcat() {
+		return lastname+firstname;
+		
+	}
+	
+}
---
+++ src/test/java/com/samplecommit/Hiding.java
@@ -0,0 +1,13 @@
+package com.samplecommit;
+
+public class Hiding {
+
+	public static void main(String[] args) {
+		Encapsulation e = new Encapsulation();
+		e.setconcat("Lishitha", "NallaReddy");
+		System.out.println("Name:"+e.getconcat());
+		
+
+	}
+
+}
